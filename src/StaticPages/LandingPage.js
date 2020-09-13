74503b37356012efd58786b76366384f34767e79
Israel-oduguwa <adeboyega22@gmail.com>
2020-09-13 21:14:15 +0100

    implemented the  react pdf

--- src/StaticPages/LandingPage.js
+++ src/StaticPages/LandingPage.js
@@ -6,8 +6,18 @@ import RightIntro from "../LandingPageComponents/RightIntro";
 import LandingPageStep2 from "../LandingPageComponents/LandingPageStep2";
 import LandingPageStep3 from "../LandingPageComponents/LandingPageStep3";
 import LandingPageStep4 from "../LandingPageComponents/LandingPageStep4";
+import { PDFDownloadLink } from "@react-pdf/renderer";
+import { PdfDocument } from "../LandingPageComponents/projotype";
 
 export class LandingPage extends Component {
+    state={
+        auth:false
+    }
+    auth = () =>{
+        this.setState({
+            auth:true
+        })
+    }
     render() {
         const toggle = this.props.darkToggle
         return (
@@ -34,6 +44,20 @@ export class LandingPage extends Component {
                 <div className="LandingPage2">
                    <LandingPageStep4/>
                 </div>
+             
+                   <PDFDownloadLink
+                   document={<PdfDocument data={1}/>}
+                   fileName="movielist.pdf"
+                   style={{
+                   textDecoration: "none",
+                   padding: "10px",
+                   color: "#4a4a4a",
+                   backgroundColor: "#f2f2f2",
+                   border: "1px solid #4a4a4a"
+                   }}
+                   >
+               Download
+                   </PDFDownloadLink>
                
                 </StaticPageNavBar>
             </div>
